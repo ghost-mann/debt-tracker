fa6162e79ac50c2175be972beaf74eb1e241f90d
ghost-mann <iamaob8@protonmail.com>
2025-06-19 13:45:37 +0300

    feat: added aiven db configuration and created sqlalchemy engine to store df in postgre in main.py

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 import requests
 import pandas as pd
+import os
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+AIVEN_USER = os.getenv('AIVEN_USER')
+AIVEN_PASSWORD = os.getenv('AIVEN_PASSWORD')
+AIVEN_HOST = os.getenv('AIVEN_HOST')
+AIVEN_PORT = os.getenv('AIVEN_PORT')
+AIVEN_DBNAME = os.getenv('AIVEN_DBNAME')
+
 
 debt_url = 'https://api.worldbank.org/v2/country/KE/indicator/DT.DOD.DECT.CD.?format=json'
 
@@ -20,3 +33,6 @@ df = df.dropna(axis=0)
 
 
 print(df)
+
+engine = create_engine(f'postgresql://{AIVEN_USER}:{AIVEN_PASSWORD}@{AIVEN_HOST}:{AIVEN_PORT}/{AIVEN_DBNAME}?sslmode=require')
+df.to_sql('external_debt', con=engine, if_exists='replace', index=False)
