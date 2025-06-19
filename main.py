d85a7541cc6a73cc229457b621130a44fccb0400
ghost-mann <iamaob8@protonmail.com>
2025-06-19 11:48:57 +0300

    feat: dropped rows with missing values in main.py

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 import requests
+import pandas as pd
 
-debt_url = 'https://api.worldbank.org/v2/country/all/indicator/DT.DOD.DECT.CD.?format=json'
+debt_url = 'https://api.worldbank.org/v2/country/KE/indicator/DT.DOD.DECT.CD.?format=json'
 
 response = requests.get(debt_url)
-data = response.json()
-print(data)
+json_data = response.json()
 
+records = json_data[1]
+
+df = pd.DataFrame([{
+    'country': d['country']['value'],
+    'date': d['date'],
+    'value': d['value']
+} for d in records])
+
+
+# drop rows with missing values
+df = df.dropna(axis=0)
+
+
+print(df)
